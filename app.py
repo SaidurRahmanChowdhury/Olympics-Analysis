8a1d659ac828ebc211b0c865078a0f18b41d6018
Saidur Rahman Chowdhury <saidur.rahman.chowdhury98@gmail.com>
2023-08-28 18:13:55 +0600

    app.py updated

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ st.set_page_config(
 # Load and preprocess data
 df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
+
 df = preprocessor.preprocess(df, region_df)
 
 st.sidebar.title("Olympics Analysis")
@@ -20,10 +21,6 @@ user_menu = st.sidebar.radio(
     'Select an Option',
     ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
 )
-
-
-# Display content based on user selection
-st.dataframe(df)
 
 if user_menu == 'Medal Tally':
     
